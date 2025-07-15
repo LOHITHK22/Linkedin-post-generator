796a7f98ae6b4e44f9edaeb21ea54d6cfa952a29
LOHITH KOVURI <147120604+LOHITHK22@users.noreply.github.com>
2025-07-15 17:02:50 -0400

    Update llm_engine.py

--- llm_engine.py
+++ llm_engine.py
@@ -1,8 +1,8 @@
 from langchain_groq import ChatGroq
 import os
-from dotenv import load_dotenv
+'''from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()'''
 llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama3-70b-8192")
 
 
